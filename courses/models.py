95a8767075ce425f5d02d864d5b43e8444549788
Darnell Holder <darnelho1@gmail.com>
2017-06-12 15:35:00 -0700

    made migrations to include the course app

---
+++ courses/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+    dateCreated = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length = 255)
+    descript = models.TextField()
+    
